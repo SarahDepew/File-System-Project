bf933dc46c4e473c9fd7e513d070ff0590d33434
Sarah Depew <sdepew@brynmawr.edu>
2018-04-30 11:19:25 -0400

    Valgrind Errors

--- format.c
+++ format.c
@@ -46,10 +46,11 @@ void close_disk() {
 void write_boot_block(FILE *disk) {
     //string has a null at the end so should add 1 to the size //TODO: I don't think this matters...
     char *boot = malloc(SIZEOFBOOTBLOCK* sizeof(char) + 1);
-    memset(boot, 0, SIZEOFBOOTBLOCK);
-    boot = "bootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootboot";
-    fwrite(boot, strlen(boot), 1, disk);
+    memset(boot, 0, SIZEOFBOOTBLOCK + 1);
     free(boot);
+//    boot = "bootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootbootboot";
+//    fwrite(boot, SIZEOFBOOTBLOCK, 1, disk);
+
 }
 
 void write_padding(FILE *disk, int amount_padding) {
