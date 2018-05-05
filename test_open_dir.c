e2ca718d06616837f3aa4603aeaa8519ec3091a6
Sarah Depew <star.1776@gmail.com>
2018-05-04 20:51:11 -0400

    fstats

--- test_open_dir.c
+++ test_open_dir.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 
 int main() {
+  // FILE *disk = fopen("DISKDIR", "r");
+  // inode *block = malloc(BLOCKSIZE);
+  // fseek(disk, SIZEOFBOOTBLOCK + SIZEOFSUPERBLOCK + 2 * sizeof(inode), SEEK_SET);
+  // fread(block, sizeof(inode), 1, disk);
+
   setup();
   printf("%s\n", "-------------end set up--------");
   f_mount("DISKDIR", "", 0);
@@ -15,8 +20,14 @@ int main() {
   printf("%s\n", "------------end f_open3----------");
   // f_opendir("/a/s/d/sf/'we'/5/46/");
   printf("fd: %d\n", fd);
-  void* buffer = malloc(200);
-  f_read(buffer, 6, 1, fd);
-  printf("%s\n", (char*) buffer);
+  stat *stats = malloc(sizeof(stat));
+  f_stat("/user/test.txt", stats);
+  printf("stats values %d, %d\n", stats->size, stats->inode_index);
+  // void* buffer = malloc(200);
+  // f_read(buffer, 5, 1, 1);
+  // inode *i = get_inode(2);
+  // void *block = get_data_block(2);
+  // printf("%s\n", (char*) buffer);
+  // printf("Contents of block: %s\n", (char*) block);
   shutdown();
 }
