cafd82e1353809ace4a3e9490b2e0f49fd397faa
Jiayi Lin <rlin1@brynmawr.edu>
2018-05-10 17:24:14 -0400

    cd directly to root

--- shell.h
+++ shell.h
@@ -175,6 +175,7 @@ int rm_builtin(char **args);
 int mount_builtin(char **args);
 int unmount_builtin(char **args);
 
+directory_entry* goto_root();
 directory_entry* goto_destination(char* filepath);
 char* convert_absolute(char* filepath);
 #endif //HW7_SHELL_H
